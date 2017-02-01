87ae28390cdce4d3bf8cb626b6ea607e2371a9ac
Matthew Kuch <kuch.matthew@gmail.com>
2017-02-01 10:46:51 +0300

    Revised order models

--- orders/admin.py
+++ orders/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import Order
+from .models import *
 
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['owner', 'district', 'month', 'submitter', 'title_submitter']
 
+
     
+
+admin.site.register(District)
+admin.site.register(Vaccine)
 admin.site.register(Order)
+admin.site.register(OrderLine)
--- orders/models.py
+++ orders/models.py
@@ -2,23 +2,45 @@ from django.db import models
 
 from datetime import date
 
-class Order(models.Model):
 
+class Vaccine(models.Model):
+	name = models.CharField(max_length=50)
+
+
+class District(models.Model):
+	name = models.CharField(max_length=50)
+
+
+
+class Order(models.Model):
 	owner = models.ForeignKey('account.Profile')
-	district = models.CharField(max_length=50, default="e.g. Abim")
+	district = models.ForeignKey(District, on_delete=models.SET_NULL, null=True, blank=True)
 	month = models.CharField(max_length=50, default="e.g. January")
 	submitter = models.CharField(max_length=50, default="e.g. Elton John Mabirizi")
 	title_submitter = models.CharField(max_length=50, default="e.g. DCCT")
+
 	#dateof_submit = models.DateField()
 	class Meta:
 		verbose_name = 'OrderGeneralInfo'
 		verbose_name_plural = 'OrderGeneralInfos'
+
+
+class OrderLine(models.Model):
+	order = models.ForeignKey(Order)
+	vaccine = models.ForeignKey(Vaccine, on_delete=models.SET_NULL, null=True, blank=True)
+	stock_balance = models.IntegerField(default=0)
+	comment = models.CharField(max_length=250) 
+
+	class Meta(object):
+		unique_together = ("order", "vaccine")	
+
 	
-	#MEASLES
-	#sbmeasles = models.IntegerField(blank=False, null=False, default=0)
-	#oqmeasles = models.IntegerField(blank=False, null=False, default=0)
-	#aomeasles = models.IntegerField(blank=False, null=False, default=0)
-	#commmeasles = models.CharField(max_length=500, default="Add comment here")
+# class OrderMeasles(models.Model):
+# 	#MEASLES
+# 	sbmeasles = models.IntegerField(blank=False, null=False, default=0)
+# 	oqmeasles = models.IntegerField(blank=False, null=False, default=0)
+# 	aomeasles = models.IntegerField(blank=False, null=False, default=0)
+# 	commmeasles = models.CharField(max_length=500, default="Add comment here")
 
 	#BCG
 	#sbbcg = models.IntegerField(blank=False, null=False, default=0)
@@ -61,5 +83,71 @@ class Order(models.Model):
 	#oqpcv = models.IntegerField(blank=False, null=False, default=0)
 	#aopcv = models.IntegerField(blank=False, null=False, default=0)
 	#commpcv = models.CharField(max_length=500, default="Add comment here")
+
+	#PENTA
+	#sbpenta = models.IntegerField(blank=False, null=False, default=0)
+	#oqpenta = models.IntegerField(blank=False, null=False, default=0)
+	#aopenta = models.IntegerField(blank=False, null=False, default=0)
+	#commpenta = models.CharField(max_length=500, default="Add comment here")
+
+	#IPV
+	#sbipv = models.IntegerField(blank=False, null=False, default=0)
+	#oqipv = models.IntegerField(blank=False, null=False, default=0)
+	#aoipv = models.IntegerField(blank=False, null=False, default=0)
+	#commipv = models.CharField(max_length=500, default="Add comment here")
+
+    #ROTA
+	#sbrota = models.IntegerField(blank=False, null=False, default=0)
+	#oqrota = models.IntegerField(blank=False, null=False, default=0)
+	#aorota = models.IntegerField(blank=False, null=False, default=0)
+	#commrota = models.CharField(max_length=500, default="Add comment here")
+
+    #Syringes 0.05ml
+	#sbs005 = models.IntegerField(blank=False, null=False, default=0)
+	#oqs005 = models.IntegerField(blank=False, null=False, default=0)
+	#aos005 = models.IntegerField(blank=False, null=False, default=0)
+	#comms005 = models.CharField(max_length=500, default="Add comment here")
+
+    #Syringes 0.5ml
+	#sbs05 = models.IntegerField(blank=False, null=False, default=0)
+	#oqs05 = models.IntegerField(blank=False, null=False, default=0)
+	#aos05 = models.IntegerField(blank=False, null=False, default=0)
+	#comms05 = models.CharField(max_length=500, default="Add comment here")
+
+    #Syringes 5ml
+	#sbs5 = models.IntegerField(blank=False, null=False, default=0)
+	#oqs5 = models.IntegerField(blank=False, null=False, default=0)
+	#aos5 = models.IntegerField(blank=False, null=False, default=0)
+	#comms5 = models.CharField(max_length=500, default="Add comment here")
+
+    #Syringes 2ml
+	#sbs2 = models.IntegerField(blank=False, null=False, default=0)
+	#oqs2 = models.IntegerField(blank=False, null=False, default=0)
+	#aos2 = models.IntegerField(blank=False, null=False, default=0)
+	#comms2 = models.CharField(max_length=500, default="Add comment here")
+
+    #Safety Boxes
+	#sbsb = models.IntegerField(blank=False, null=False, default=0)
+	#oqsb = models.IntegerField(blank=False, null=False, default=0)
+	#aosb = models.IntegerField(blank=False, null=False, default=0)
+	#commsb = models.CharField(max_length=500, default="Add comment here")
+
+    #Tetanus Toxoid (TT) Cards
+	#sbttc = models.IntegerField(blank=False, null=False, default=0)
+	#oqttc = models.IntegerField(blank=False, null=False, default=0)
+	#aottc = models.IntegerField(blank=False, null=False, default=0)
+	#commttc = models.CharField(max_length=500, default="Add comment here")
+
+    #Child Health Cards (CHC)
+	#sbchc = models.IntegerField(blank=False, null=False, default=0)
+	#oqchc = models.IntegerField(blank=False, null=False, default=0)
+	#aochc = models.IntegerField(blank=False, null=False, default=0)
+	#commchc = models.CharField(max_length=500, default="Add comment here")
+
+    #Vaccine Control Book (VCB)
+	#sbvcb = models.IntegerField(blank=False, null=False, default=0)
+	#oqvcb = models.IntegerField(blank=False, null=False, default=0)
+	#aovcb = models.IntegerField(blank=False, null=False, default=0)
+	#commvcb = models.CharField(max_length=500, default="Add comment here")
 
 
